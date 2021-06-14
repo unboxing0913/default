f73eef90e6e61eac839f744600505d985c819723
unboxing <minus0913@daum.net>
2021-06-14 18:04:49 +0900

    first up

---
+++ ex00/src/main/java/org/zerock/sample/Hotel.java
@@ -0,0 +1,19 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class Hotel {
+	
+	@NonNull
+	private Chef chef;
+	
+}
